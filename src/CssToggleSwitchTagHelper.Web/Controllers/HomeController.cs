5aca28d0ed342296ca8f8d9fff5eadfbec8440fd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-01 23:02:55 +0100

    Simplify project structure

--- src/CssToggleSwitchTagHelper.Web/Controllers/HomeController.cs
+++ src/CssToggleSwitchTagHelper.Web/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 namespace CssToggleSwitchTagHelper.Web.Controllers
@@ -10,20 +6,6 @@ namespace CssToggleSwitchTagHelper.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
